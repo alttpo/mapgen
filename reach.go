ce6986237f17f7ae8bef840fe8d2e17bcaed68b0
James Dunne <jim.dunne@ukg.com>
2023-09-22 20:51:16 -0500

    progress made on reachability; traversing doors, stairs, warps

---
+++ reach.go
@@ -0,0 +1,228 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func reachabilityAnalysis(initEmu *System) (err error) {
+	e := &System{}
+	if err = e.InitEmulatorFrom(initEmu); err != nil {
+		panic(err)
+	}
+
+	// find Underworld_LoadHeader routine to check the RoomHeader pointer it uses
+	// analyze the overlaps of the room headers and mark invalid any overlapping values
+	// iterate through entrance IDs to determine dungeon IDs
+	// iterate through supertile links
+
+	// RoomHeader_RoomToPointer#_04F1E2
+	// RoomHeader_Room0000#_04F462
+
+	// #_01B5DC: LDA.l RoomHeader_RoomToPointer,X
+	if e.Bus.Read8(fastRomBank|0x01_b5dc) != 0xBF {
+		panic("unexpected opcode at #_01B5DC; expecting `LDA.l $nnnnnn,X`")
+	}
+	roomHeaderTableLong := e.Bus.Read24(fastRomBank | 0x01_b5dc + 1)
+	//fmt.Printf("table: %06x\n", roomHeaderTableLong)
+
+	aliases := map[uint16]uint16{}
+	roomHeaderPointers := [0x140]uint16{}
+	for i := uint16(0); i < 0x140; i++ {
+		roomHeaderPointers[i] = e.Bus.Read16(roomHeaderTableLong + uint32(i)<<1)
+		//fmt.Printf("[%03x]: %04x\n", i, roomHeaderPointers[i])
+
+		// find our alias:
+		for j := uint16(0); j < i; j++ {
+			if roomHeaderPointers[i] == roomHeaderPointers[j] {
+				aliases[i] = j
+				//fmt.Printf("  alias for %03x\n", j)
+				break
+			}
+		}
+	}
+
+	// make a map of which room index owns which bytes in the table (earliest room ID "wins"):
+	addrUsed := map[uint16]uint16{}
+	for i := uint16(0); i < 0x140; i++ {
+		// skip overlap analysis for aliased pointers:
+		if _, ok := aliases[i]; ok {
+			continue
+		}
+
+		for j := uint16(0); j < 14; j++ {
+			p := roomHeaderPointers[i] + j
+			addrUsed[p] = i
+		}
+	}
+
+	roomHeaders := [0x140][14]uint8{}
+	for i := uint16(0); i < 0x140; i++ {
+		owner := i
+		if alias, ok := aliases[owner]; ok {
+			owner = alias
+		}
+		for j := uint16(0); j < 14; j++ {
+			p := roomHeaderPointers[owner] + j
+			if addrUsed[p] == owner {
+				roomHeaders[i][j] = e.Bus.Read8(roomHeaderTableLong&0xFF_0000 | uint32(p))
+			}
+		}
+	}
+
+	//for i := uint16(0); i < 0x140; i++ {
+	//	fmt.Printf("[%03x]: %#v\n", i, roomHeaders[i])
+	//}
+
+	wram := (e.WRAM)[:]
+
+	type Dungeon struct {
+		DungeonID         uint8
+		Entrances         []uint8
+		Supertiles        []Supertile
+		ContainsSupertile map[uint16]struct{}
+
+		Stack []Supertile
+	}
+	dungeons := map[uint8]*Dungeon{}
+
+	// entrances...
+	for eID := uint8(0); eID < 0x85; eID++ {
+		// poke the entrance ID into our asm code:
+		e.HWIO.Dyn[setEntranceIDPC&0xffff-0x5000] = eID
+
+		// load the entrance:
+		if err = e.ExecAt(loadEntrancePC, donePC); err != nil {
+			panic(err)
+		}
+		entranceLoadedWRAM := *e.WRAM
+
+		// read dungeon ID:
+		dungeonID := read8(wram, 0x040C)
+		fmt.Printf("dungeon %02x\n", dungeonID)
+
+		// fetch or create dungeon record:
+		dungeon, ok := dungeons[dungeonID]
+		if !ok {
+			dungeon = &Dungeon{
+				DungeonID:         dungeonID,
+				Entrances:         nil,
+				Supertiles:        nil,
+				ContainsSupertile: make(map[uint16]struct{}),
+				Stack:             make([]Supertile, 0, 40),
+			}
+			dungeons[dungeonID] = dungeon
+		}
+		dungeon.Entrances = append(dungeons[dungeonID].Entrances, eID)
+
+		// read initial entrance supertile:
+		st := read16(wram, 0xA0)
+		dungeon.Stack = append(dungeon.Stack, Supertile(st))
+
+		for len(dungeon.Stack) > 0 {
+			// pop off the stack:
+			lifoEnd := len(dungeon.Stack) - 1
+			st := dungeon.Stack[lifoEnd]
+			dungeon.Stack = dungeon.Stack[0:lifoEnd]
+
+			// skip if already scanned:
+			st16 := uint16(st)
+			if _, ok := dungeon.ContainsSupertile[st16]; ok {
+				continue
+			}
+
+			fmt.Printf("  scan supertile %03x\n", st16)
+			dungeon.ContainsSupertile[st16] = struct{}{}
+			dungeon.Supertiles = append(dungeon.Supertiles, st)
+
+			// follow supertile links to WARP, STAIR0, STAIR1, STAIR2, STAIR3:
+			for j := 9; j < 14; j++ {
+				dest := uint16(roomHeaders[st16][j])
+				if dest == 0 {
+					continue
+				}
+
+				// make sure we haven't already scanned the supertile:
+				if _, ok := dungeon.ContainsSupertile[dest]; ok {
+					continue
+				}
+
+				// add it to the stack to be scanned for links:
+				dungeon.Stack = append(dungeon.Stack, Supertile(dest))
+			}
+
+			// load the supertile into emulator memory:
+			*e.WRAM = entranceLoadedWRAM
+
+			// set supertile:
+			write16(wram, 0xA0, uint16(st))
+			write16(wram, 0x048E, uint16(st))
+
+			//e.LoggerCPU = e.Logger
+			if err = e.ExecAt(loadSupertilePC, donePC); err != nil {
+				return
+			}
+			//e.LoggerCPU = nil
+
+			tiles := wram[0x12000:0x14000]
+			//bg1wram := (*(*[0x1000]uint16)(unsafe.Pointer(&wram[0x2000])))[:]
+			//bg2wram := (*(*[0x1000]uint16)(unsafe.Pointer(&wram[0x4000])))[:]
+			_ = os.WriteFile(fmt.Sprintf("data/t%03x.map", st16), tiles, 0644)
+
+			// check doors to neighboring supertiles:
+			for m := 0; m < 16; m++ {
+				tpos := read16(wram, uint32(0x19A0+(m<<1)))
+				// stop marker:
+				if tpos == 0 {
+					//fmt.Printf("  door stop at marker\n")
+					break
+				}
+
+				door := Door{
+					Pos:  MapCoord(tpos >> 1),
+					Type: DoorType(read16(wram, uint32(0x1980+(m<<1)))),
+					Dir:  Direction(read16(wram, uint32(0x19C0+(m<<1)))),
+				}
+				doorTile := tiles[door.Pos+0x41]
+
+				fmt.Printf("  door: %v; t = %02x\n", door, doorTile)
+
+				// check if the door is on the edge:
+				isDoorEdge, edgeDir, _, _ := door.Pos.IsDoorEdge()
+				if !isDoorEdge {
+					continue
+				}
+
+				// don't traverse over dungeon-exit doors:
+				if doorTile == 0x8e {
+					// south dungeon exit:
+					continue
+				}
+				if doorTile == 0x89 {
+					// east/west exit:
+					continue
+				}
+
+				// attempt to move through the door to the neighboring supertile:
+				var dest Supertile
+				var ok bool
+				if dest, _, ok = st.MoveBy(edgeDir); !ok {
+					continue
+				}
+
+				// make sure we haven't already scanned the neighboring supertile:
+				dest16 := uint16(dest)
+				if _, ok = dungeon.ContainsSupertile[dest16]; ok {
+					continue
+				}
+
+				// push neighbor onto supertile stack:
+				dungeon.Stack = append(dungeon.Stack, dest)
+			}
+		}
+
+		fmt.Printf("d[%02x]: %#v\n", dungeon.DungeonID, dungeon.Supertiles)
+	}
+
+	return
+}
